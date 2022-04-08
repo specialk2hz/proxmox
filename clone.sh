7c46f5ef501e16f38d2410de070b672d670d11ae
root <root@polaris.home.cnaa>
2022-04-08 14:38:48 -0400

    fixed spacing, re-enabled all commands

--- clone.sh
+++ clone.sh
@@ -3,13 +3,13 @@
 # clear
 # echo "#############################################################################################"
 # echo "###"
-# echo "###    CNAA Proxmox Virtual Machine Builder Script"
+# echo "###    CNAA Proxmox Clone Script"
 # echo "###"
 # echo "#############################################################################################"
 # echo
 # echo
 
-# read -p "Template ID to clone? " template_id
+read -p "Template ID to clone? " template_id
 read -p "VMID for Clone? " clone_id
 read -p "VM Name? " clone_name
 read -p "What Storage? sata, nvme) " clone_storage
@@ -20,7 +20,7 @@ read -p "DHCP? [yn] " answer
 if [ "$answer" == "y" ];
 then
   echo "Clone $clone_name ($clone_id) Complete!"
-  read -p "Start the VM?" answer
+  read -p "Start the VM? " answer
   if [ "$answer" == "y" ];
   then
     echo "Starting $clone_name ($clone_id) "
@@ -35,7 +35,7 @@ read -p "IP Address? " clone_ip
 fi
 
 read -p "Subnet Mask /24? [yn] " answer
-if [[ $answer = y ]] ; then
+if [[ $answer = "y" ]] ; then
 clone_subnet=24
 else
 echo -n "Enter Subnet Mask (/XX)? "
@@ -43,7 +43,7 @@ read clone_subnet
 fi
 
 read -p "Gateway 10.10.10.1? [yn] " answer
-if [[ $answer = y ]] ; then
+if [[ $answer = "y" ]] ; then
 clone_gateway=10.10.10.1
 else
 echo -n "Gateway? "
@@ -52,7 +52,7 @@ fi
 
 qm set $clone_id --ipconfig0 ip=$clone_ip/$clone_subnet,gw=$clone_gateway
 
-read -p "Start the VM? " answer
+read -p "Start the VM?  " answer
 if [ "$answer" == "y" ];
 then
     echo "Starting $clone_name ($clone_id) "
